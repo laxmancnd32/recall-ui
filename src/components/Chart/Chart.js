2767973aeb554f648362a6e6d4a25c237163914d
swarna.p.rout <swarna.p.rout@BLR1-LMAC-N60587.local>
2019-04-27 09:14:44 +0530

    added graph

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import {Line as LineChart} from 'react-chartjs';
+
+function chartData() {
+  return {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+      {
+        label: 'My First dataset',
+        fillColor: 'rgba(220,220,220,0.2)',
+        strokeColor: 'rgba(220,220,220,1)',
+        pointColor: 'rgba(220,220,220,1)',
+        pointStrokeColor: '#fff',
+        pointHighlightFill: '#fff',
+        pointHighlightStroke: 'rgba(220,220,220,1)',
+        data: [35, 36, 25, 25, 35, 55, 40],
+      }
+    ]
+  }
+}
+
+const options = {
+  scaleShowGridLines: true,
+  scaleGridLineColor: 'rgba(0,0,0,.05)',
+  scaleGridLineWidth: 1,
+  scaleShowHorizontalLines: true,
+  scaleShowVerticalLines: true,
+  bezierCurve: true,
+  bezierCurveTension: 0.4,
+  pointDot: true,
+  pointDotRadius: 4,
+  pointDotStrokeWidth: 1,
+  pointHitDetectionRadius: 20,
+  datasetStroke: true,
+  datasetStrokeWidth: 2,
+  datasetFill: true,
+  legendTemplate: '<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>',
+}
+
+const styles = {
+  graphContainer: {
+    border: '1px solid black',
+    padding: '15px',
+    marginTop: '40px',
+    width:'100%'
+  }
+}
+
+class Chart extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: chartData()
+    }
+  }
+
+  render() {
+    return (
+      <div style={styles.graphContainer}>
+        <LineChart data={this.state.data}
+          options={options}
+          width="1000" height="400"/>
+      </div>
+    )
+  }
+}
+export default Chart;
